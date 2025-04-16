4fd11c3f8bb7cd44b7a10ad7ee75b6c51f91f647
rukminigaddam <rukminirgaddam@gmail.com>
2025-04-16 16:37:53 -0400

    created the structure of the first 2 classes

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 class UserProfile():
-    pass
+    def __init__(self, age, ethnicity, family_history, genetic_marker):
+        pass
+
+    def get_summary(self):
+        pass
+
+    def has_risk_factors(self):
+        pass
+
+
+
+class RiskCalculator():
+    def __init__(self, dataset):
+        pass
+
+    def classify_risk(self, score):
+        pass
