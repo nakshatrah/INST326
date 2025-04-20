84dac443da6feecd1ec0e04bf5d079aaedfaaf4f
nakshatrah <nakshatrahiray@gmail.com>
2025-04-20 01:04:31 -0400

    Finished unit tests and saving current progress on docstrings.

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
 #class 1
 class UserProfile():
+    """ This class represents a user's health profile, storing info such as age, ethnicity, familiy history, and genetic markers. 
+    
+    Attributes:
+        age(int): The age of the user.
+        ethnicity(str): The ethnicity of the user. 
+        family_history(boolean): This tells us whether or not there is a family history of diabetes or heart disease.
+        genetic_marker(boolean): This tells us whether or not the user has a genetic marker for diabetes or heart disease.
+    """
+    
     def __init__(self, age, ethnicity, family_history, genetic_marker):
+        """This is the usual initialization function, which sets up a UserProfile object.
+        age(int): The age of the user.
+        ethnicity(str): The ethnicity of the user. 
+        family_history(boolean): This tells us whether or not there is a family history of diabetes or heart disease.
+        genetic_marker(boolean): This tells us whether or not the user has a genetic marker for diabetes or heart disease.
+        """
         pass
 
     def get_summary(self):
+        """This function returns a summary of the user's profile.
+        
+        Args:
+            None, just the self parameter that is required in all class methods.
+
+        Returns:
+            str: A summary of the user's profile in a string.
+        
+        """
         pass
 
     def has_risk_factors(self):
+        """ This function will determine if the user has any risk factors based on their profile.
+        
+        Returns:
+            boolean: This is true if the risk factors are present. If not, it is false. 
+        """
         pass
 
 
 #Class 2 test comment 
 class RiskCalculator():
+    """"""
     def __init__(self, dataset):
         pass
 
--- test_main.py
+++ test_main.py
@@ -5,6 +5,38 @@ class TestUserProfile(unittest.TestCase):
     def test_summary(self):
         user = UserProfile(24, "Hispanic", True, True)
         self.assertIsNone(user.get_summary()) 
-    
+
+    def test_risk_factors(self):
+        user = UserProfile(40, "White", False, True)
+        self.assertIsNone(user.has_risk_factors())
+
+class TestDiabetesRisk(unittest.TestCase):
+    def test_score(self):
+        user = UserProfile(50, "Asian", True, False)
+        calc = DiabetesRiskCalculator("dataset")
+        self.assertIsNone(calc.calculate_score(user))
+
+    def test_assess(self):
+        user = UserProfile(60, "Black", True, True)
+        calc = DiabetesRiskCalculator("dataset")
+        self.assertIsNone(calc.assess(user))
+
+class TestHeartRisk(unittest.TestCase):
+    def test_score(self):
+        user = UserProfile(27, "Asian", False, True)
+        calc = HeartDiseaseRiskCalculator("dataset")
+        self.assertIsNone(calc.calculate_score(user))
+
+    def test_assess(self):
+        user = UserProfile(55, "Hispanic", True, False)
+        calc = HeartDiseaseRiskCalculator("dataset")
+        self.assertIsNone(calc.assess(user))
+
+class TestRiskAssessmentSystem(unittest.TestCase):
+    def test_all(self):
+        user = UserProfile(19, "Black", True, True)
+        system = RiskAssesmentSystem(user, "dataset")
+        self.assertIsNone(system.full_assesment())
+
 if __name__ == '__main__':
     unittest.main() 
